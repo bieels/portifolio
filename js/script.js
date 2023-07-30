c20af3a5cd2d31fd8e7c46b32245045039a3ed1b
BieelS <bieelss01@gmail.com>
2023-07-29 22:53:29 -0300

    Alterações básicas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ======================= typing animation ======================= */
 var typed = new Typed(".typing", {
-  strings: ["",  "Front-End Developer"],
+  strings: ["",  "FullStack Developer"],
   typeSpeed: 100,
   BackSpeed: 60,
   loop: false,
